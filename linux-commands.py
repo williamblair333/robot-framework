e0c80af74e932865bff0e38bdbeb1f1586bbfd45
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-01-21 16:44:09 -0500

    Add files via upload

---
+++ linux-commands.py
@@ -0,0 +1,76 @@
+################################################################################
+#
+#Run example: Not intended for direct usage
+#File:      linux-commands.py
+#Date:      2021DEC17
+#Author:    William Blair
+#Contact:	williamblair333@gmail.com
+#Tested on: Windows 10
+#To test:   Debian 10+ | Ubuntu 14+
+#
+################################################################################
+import os
+import paramiko
+from robot.api.deco import keyword
+
+@keyword("Setup Key File")
+def setup_key_file(filename='secret_rsa'):
+    key_str="""-----BEGIN RSA PRIVATE KEY-----\n\
+Proc-Type: 4,ENCRYPTED\n\
+DEK-Info: AES-128-CBC,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\
+-----END RSA PRIVATE KEY-----"""
+
+    if not os.path.exists(filename) :
+        f = open(filename, "w")
+        f.write(key_str)
+        f.close()
+    else:
+        print('Key file exists')
+
+    return filename
+
+@keyword('SSH Connect To Server')
+def connect_to_server(host, filename):
+    username = 'username_here'
+    password = 'password_here'
+    ssh_key = paramiko.RSAKey.from_private_key_file(filename, password=password)
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=username, pkey=ssh_key)
+    print(f"SSH connected {host}@{username}")
+
+    return client
+
+@keyword('SSH Kill Process')
+def arm_kill_cli(ssh_connect):
+    command = 'bash -c "kill \$(ps |grep process_name | grep -v grep | sed \'s/ root.*//\')"'
+    stdin, stdout, stderr = ssh_connect.exec_command(command)
+    stdout.channel.set_combine_stderr(True)
+    read = stdout.read()
+    print(str(read))
+
+@keyword('SSH Generate File')
+def arm_generate_file(ssh_connect):
+    command = 'bash -c "echo -n pipe_txtfile > /tmp/pipe_txtfile"'
+    stdin, stdout, stderr = ssh_connect.exec_command(command)
+    stdout.channel.set_combine_stderr(True)
+    read = stdout.read()
+    print(str(read))
+
+@keyword('SSH Close Connection')
+def close_ssh_client(ssh_connect):
+    ssh_connect.close()
---
+++ linux-commands.robot
@@ -0,0 +1,38 @@
+################################################################################
+#
+#Run example: robot -v ip_host:192.168.1.123 -i invoke_tests linux-commands.robot
+#File:      linux-commands.robot
+#Date:      2021DEC17
+#Author:    William Blair
+#Contact:	williamblair333@gmail.com
+#Tested on: Windows 10
+#To test:   Debian 10+ | Ubuntu 14+
+#
+#This script is intended to do the following:
+#
+#- demo of using python functions in robot to ssh connect to server and perform
+#- various commands and then disconnect
+#
+################################################################################
+
+*** Settings ***
+Documentation  		Linux commands
+
+Library   			linux-commands.py
+
+Test Setup  		Begin Test
+Test Teardown   	End Test
+
+*** Variables ***
+${IP_HOST}		${EMPTY}
+
+*** Test Cases ***
+Invoke Tests
+    [Tags]  invoke_tests
+    [Documentation]  To demo using python functions in robot
+
+    ${filename}=  Setup Key File
+	${client}=  SSH Connect To Server   ${IP_HOST}  ${filename}
+	SSH ARM Kill Process	${client}
+	SSH ARM Generate File	${client}
+    SSH Close Connection	${client}
